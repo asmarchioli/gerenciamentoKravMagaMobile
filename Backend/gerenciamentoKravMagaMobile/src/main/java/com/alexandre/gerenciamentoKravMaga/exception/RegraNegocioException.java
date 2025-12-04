ddd9dd20df93f4001c2f3526e7d2541a9a699c0f
asmarchioli <alexandresilva.marchioli@gmail.com>
2025-12-03 22:30:14 -0300

    Commit 11 - Update backend logic and update frontend erros catch

---
+++ Backend/gerenciamentoKravMagaMobile/src/main/java/com/alexandre/gerenciamentoKravMaga/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package com.alexandre.gerenciamentoKravMaga.exception;
+
+public class RegraNegocioException extends RuntimeException {
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
